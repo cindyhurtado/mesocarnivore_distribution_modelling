5be34878e9ea390ac612156d2edb16152c8c5095
Hurtado <Cindy.Hurtado@gov.bc.ca>
2024-06-26 15:06:28 -0700

    M

--- MDM_simulations/1. Scripts/8. graphs.R
+++ MDM_simulations/1. Scripts/8. graphs.R
@@ -9,7 +9,7 @@ library(miceadds)
 
 #omineca model
 
-load("C:/LocalR/mesocarnivore_distribution_modelling/MDM_simulations/omineca_realdata_NA_OS.RData")
+load("C:/LocalR/mesocarnivore_distribution_modelling/Fisher models/2. Outputs/out.omineca_bern_1omineca_RD_bern.RData")
 
 traceplot(out.omineca_bern_1 ,param= c("p0.S", "N", "p0.O", "psi", "sigma"))
 
@@ -28,6 +28,35 @@ colnames(Pos.df) <- "Pos"
 Pos.df$Name <- "Omineca" 
 
 ggplot(Pos.df, aes(x= Name, y=Pos))+ geom_violin() + stat_summary(fun.data=mean_sdl, mult=1, geom="pointrange", color="red")
+
+# Cariboo model 
+
+load("C:/LocalR/mesocarnivore_distribution_modelling/Fisher models/2. Outputs/out.cariboo_bern_1cariboo_RD_bern.RData")
+
+traceplot(out.cariboo_bern_1 ,param= c("p0.S", "N", "p0.O", "psi", "sigma"))
+
+traceplot(out.cariboo_bern_1 ,param= "N")
+whiskerplot(out.cariboo_bern_1, parameters = c("N"))
+
+
+# Chilcotin model 
+
+load("C:/LocalR/mesocarnivore_distribution_modelling/Fisher models/2. Outputs/out.cariboo_bern_1cariboo_RD_bern.RData")
+
+traceplot(out.cariboo_bern_1 ,param= c("p0.S", "N", "p0.O", "psi", "sigma"))
+
+traceplot(out.cariboo_bern_1 ,param= "N")
+whiskerplot(out.cariboo_bern_1, parameters = c("N"))
+
+
+
+Pos.df <- as.data.frame(out.Omineca_1$sims.list$p0.S)
+colnames(Pos.df) <- "Pos"
+Pos.df$Name <- "Omineca" 
+
+ggplot(Pos.df, aes(x= Name, y=Pos))+ geom_violin() + stat_summary(fun.data=mean_sdl, mult=1, geom="pointrange", color="red")
+
+
 
 
 
